832ad1cd01021b090c43c3003d6e9ee068009cd1
Reyes Adrian Balam <adrian_db2797@hotmail.com>
2019-04-18 23:40:56 -0500

    Update main.py

--- Consulta de API/main.py
+++ Consulta de API/main.py
@@ -1,17 +1,20 @@
 import requests
 if __name__ == '__main__':	
-
-	codigo = input("Ingrese el codigo del pais"+'\n')
-
-	url = 'https://restcountries.eu/rest/v2/alpha/'+codigo
-	response = requests.get(url)
+	class api:
+		def consulta(self, codigo):
+			url = 'https://restcountries.eu/rest/v2/alpha/'+codigo
+			response = requests.get(url)
 	
-	if response.status_code == 200:
+			if response.status_code == 200:
 		
-		response_json = response.json()
-		nombre = response_json['name']
-		bandera = response_json['flag']
-		capital = response_json['capital']
-		poblacion = response_json['population']
-		print(nombre+'\n'+capital+'\n'+bandera)
-		print(poblacion)
+				response_json = response.json()
+				nombre = response_json['name']
+				bandera = response_json['flag']
+				capital = response_json['capital']
+				poblacion = response_json['population']
+				print(nombre+'\n'+capital+'\n'+bandera)
+				print(poblacion)
+
+inicio = api()
+pais = input("Ingrese el codigo del pais"+'\n')
+inicio.consulta(pais)
