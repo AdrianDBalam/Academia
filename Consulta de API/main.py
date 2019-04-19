e4412e7ef8277476e997e14dc9e45aee8e09bb7f
Reyes Adrian Balam <adrian_db2797@hotmail.com>
2019-04-18 22:58:13 -0500

    Add files via upload

---
+++ Consulta de API/main.py
@@ -0,0 +1,17 @@
+import requests
+if __name__ == '__main__':	
+
+	codigo = input("Ingrese el codigo del pais"+'\n')
+
+	url = 'https://restcountries.eu/rest/v2/alpha/'+codigo
+	response = requests.get(url)
+	
+	if response.status_code == 200:
+		
+		response_json = response.json()
+		nombre = response_json['name']
+		bandera = response_json['flag']
+		capital = response_json['capital']
+		poblacion = response_json['population']
+		print(nombre+'\n'+capital+'\n'+bandera)
+		print(poblacion)
